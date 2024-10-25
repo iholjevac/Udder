fabac7c729283224b1258c74c14b18d0d944d148
Ivana Holjevac <ivanah794@gmail.com>
2024-10-25 07:40:30 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ivana  Holjevac on 25.10.2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
